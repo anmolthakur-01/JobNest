104094383a8cc4d8961bb0d4e6f10b65d299f6fc
Anmol Thakur <at2314541@gmail.com>
2025-03-02 23:05:58 +0530

    n

---
+++ backend/server/jobPost/jobPostModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const jobCategorySchema = mongoose.Schema({
+  categoryName: { type: String, default: null },
+  description: { type: String, default: null },
+  status: { type: Boolean, default: true },
+  createAt: { type: Date, default: Date.now() },
+});
+
+module.exports = mongoose.model("category", jobCategorySchema);
