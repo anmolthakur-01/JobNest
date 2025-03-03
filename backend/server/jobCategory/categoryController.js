7e138f63a149935588b2c68c5e7722d436434c88
Anmol Thakur <at2314541@gmail.com>
2025-03-03 17:07:38 +0530

    set validatoinerror

--- backend/server/jobCategory/categoryController.js
+++ backend/server/jobCategory/categoryController.js
@@ -64,88 +64,121 @@ const getAllCategory = (req, res) => {
 };
 
 const getSingleCategory = (req, res) => {
-  Category.findOne({ _id: req.body._id })
-    .then((data) => {
-      res.send({
-        status: 200,
-        success: true,
-        message: "Data loaded",
-        data: data,
-      });
-    })
-    .catch((err) => {
-      res.send({
-        status: 500,
-        success: false,
-        message: "Internal server error!",
-        error: err.message,
-      });
+  var validationerror = [];
+  if (!req.body._id) validationerror.push("_id is required.");
+  if (validationerror.length > 0) {
+    res.send({
+      status: 420,
+      success: false,
+      message: "Data not found!",
+      error: validationerror,
     });
+  } else {
+    Category.findOne({ _id: req.body._id })
+      .then((data) => {
+        res.send({
+          status: 200,
+          success: true,
+          message: "Data loaded",
+          data: data,
+        });
+      })
+      .catch((err) => {
+        res.send({
+          status: 500,
+          success: false,
+          message: "Internal server error!",
+          error: err.message,
+        });
+      });
+  }
 };
 
 const updateCategory = (req, res) => {
-  Category.findOne({ _id: req.body._id })
-    .then((categoryData) => {
-      if (!categoryData) {
-        res.send({
-          status: 420,
-          success: false,
-          message: "Data not found!",
-          data: categoryData,
-        });
-      } else {
-        if (req.body.categoryName)
-          categoryData.categoryName = req.body.categoryName;
-        if (req.body.description)
-          categoryData.description = req.body.description;
-        categoryData
-          .save()
-          .then((data) => {
-            res.send({
-              status: 200,
-              success: true,
-              message: "Data updated successfully",
-              data: data,
-            });
-          })
-          .catch((err) => {
-            res.send({
-              status: 500,
-              success: false,
-              message: "Internal server error!",
-              error: err.message,
-            });
-          });
-      }
-    })
-    .catch((err) => {
-      res.send({
-        status: 500,
-        success: false,
-        message: "Internal server error!",
-        error: err.message,
-      });
+  var validationerror = [];
+  if (!req.body._id) validationerror.push("_id is required.");
+  if (validationerror.length > 0) {
+    res.send({
+      status: 420,
+      success: false,
+      message: "Data not found!",
+      error: validationerror,
     });
+  } else {
+    Category.findOne({ _id: req.body._id })
+      .then((categoryData) => {
+        if (!categoryData) {
+          res.send({
+            status: 420,
+            success: false,
+            message: "Data not found!",
+            data: categoryData,
+          });
+        } else {
+          if (req.body.categoryName)
+            categoryData.categoryName = req.body.categoryName;
+          if (req.body.description)
+            categoryData.description = req.body.description;
+          categoryData
+            .save()
+            .then((data) => {
+              res.send({
+                status: 200,
+                success: true,
+                message: "Data updated successfully",
+                data: data,
+              });
+            })
+            .catch((err) => {
+              res.send({
+                status: 500,
+                success: false,
+                message: "Internal server error!",
+                error: err.message,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        res.send({
+          status: 500,
+          success: false,
+          message: "Internal server error!",
+          error: err.message,
+        });
+      });
+  }
 };
 
 const deleteCategory = (req, res) => {
-  Category.deleteOne({ _id: req.body._id })
-    .then((categoryData) => {
-      res.send({
-        status: 200,
-        success: true,
-        message: "Deleted successfully",
-        data: categoryData,
-      });
-    })
-    .catch((err) => {
-      res.send({
-        status: 500,
-        success: false,
-        message: "Internal server error!",
-        error: err.message,
-      });
+  var validationerror = [];
+  if (!req.body._id) validationerror.push("_id is required.");
+  if (validationerror.length > 0) {
+    res.send({
+      status: 420,
+      success: false,
+      message: "Data not found!",
+      error: validationerror,
     });
+  } else {
+    Category.deleteOne({ _id: req.body._id })
+      .then((categoryData) => {
+        res.send({
+          status: 200,
+          success: true,
+          message: "Deleted successfully",
+          data: categoryData,
+        });
+      })
+      .catch((err) => {
+        res.send({
+          status: 500,
+          success: false,
+          message: "Internal server error!",
+          error: err.message,
+        });
+      });
+  }
 };
 
 module.exports = {
