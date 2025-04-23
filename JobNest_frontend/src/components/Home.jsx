a1937594f68801eee20a0a3d72c9ecc137749010
Anmol Thakur <at2314541@gmail.com>
2025-04-23 14:39:39 +0530

    changes

--- JobNest_frontend/src/components/Home.jsx
+++ JobNest_frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import Hero from "./Hero";
 import CategoryCarousel from "./CategoryCarousel";
+import Jobs from "./Jobs";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <CategoryCarousel />
+      <Jobs/>
     </>
   );
 };
